941efe04b3c8101dcb7f4957c122f0d7daa6c7e1
b2rsp <vitorgoncalves57@gmail.com>
2018-01-27 15:24:15 +0000

    Added more react to it

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -7,7 +7,6 @@ import Message from './Message.js';
 class Chatroom extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             chats: [{
                 username: "Kevin Hsu",
@@ -17,33 +16,8 @@ class Chatroom extends React.Component {
                 username: "Alice Chen",
                 content: <p>Love it! :heart:</p>,
                 img: "http://i.imgur.com/Tj5DGiO.jpg",
-            }, {
-                username: "Kevin Hsu",
-                content: <p>Check out my Github at https://github.com/WigoHunter</p>,
-                img: "http://i.imgur.com/Tj5DGiO.jpg",
-            }, {
-                username: "KevHs",
-                content: <p>Lorem ipsum dolor sit amet, nibh ipsum. Cum class sem inceptos incidunt sed sed. Tempus wisi enim id, arcu sed lectus aliquam, nulla vitae est bibendum molestie elit risus.</p>,
-                img: "http://i.imgur.com/ARbQZix.jpg",
-            }, {
-                username: "Kevin Hsu",
-                content: <p>So</p>,
-                img: "http://i.imgur.com/Tj5DGiO.jpg",
-            }, {
-                username: "Kevin Hsu",
-                content: <p>Chilltime is going to be an app for you to view videos with friends</p>,
-                img: "http://i.imgur.com/Tj5DGiO.jpg",
-            }, {
-                username: "Kevin Hsu",
-                content: <p>You can sign-up now to try out our private beta!</p>,
-                img: "http://i.imgur.com/Tj5DGiO.jpg",
-            }, {
-                username: "Alice Chen",
-                content: <p>Definitely! Sounds great!</p>,
-                img: "http://i.imgur.com/Tj5DGiO.jpg",
             }]
         };
-
         this.submitMessage = this.submitMessage.bind(this);
     }
 
@@ -61,7 +35,6 @@ class Chatroom extends React.Component {
 
     submitMessage(e) {
         e.preventDefault();
-
         this.setState({
             chats: this.state.chats.concat([{
                 username: "Kevin Hsu",
@@ -79,7 +52,6 @@ class Chatroom extends React.Component {
 
         return (
             <div className="chatroom">
-                <h3>Chilltime</h3>
                 <ul className="chats" ref="chats">
                     {
                         chats.map((chat) => 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import setupSocket from './sockets'
+
+var username = 'vitor'
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, { socket, username })
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
